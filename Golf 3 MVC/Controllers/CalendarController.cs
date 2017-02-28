449f218c9a51baf6b59910072b06943a8eb1a1f6
Tonny Svensson <tonnysvensson85@hotmail.se>
2017-02-28 10:01:20 +0100

    Schema

---
+++ Golf 3 MVC/Controllers/CalendarController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using DHTMLX.Scheduler;
+using DHTMLX.Common;
+using DHTMLX.Scheduler.Data;
+using DHTMLX.Scheduler.Controls;
+
+using Golf_3_MVC.Models;
+namespace Golf_3_MVC.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Being initialized in that way, scheduler will use CalendarController.Data as a the datasource and CalendarController.Save to process changes
+            var scheduler = new DHXScheduler(this);
+
+            /*
+             * It's possible to use different actions of the current controller
+             *      var scheduler = new DHXScheduler(this);     
+             *      scheduler.DataAction = "ActionName1";
+             *      scheduler.SaveAction = "ActionName2";
+             * 
+             * Or to specify full paths
+             *      var scheduler = new DHXScheduler();
+             *      scheduler.DataAction = Url.Action("Data", "Calendar");
+             *      scheduler.SaveAction = Url.Action("Save", "Calendar");
+             */
+
+            /*
+             * The default codebase folder is ~/Scripts/dhtmlxScheduler. It can be overriden:
+             *      scheduler.Codebase = Url.Content("~/customCodebaseFolder");
+             */
+            
+
+            scheduler.LoadData = true;
+            scheduler.EnableDataprocessor = true;
+
+            return View(scheduler);
+        }
+
+        public ContentResult Data()
+        {
+            var data = new SchedulerAjaxData(new dsu3_dsu3DataContext().boknings);
+
+            return (ContentResult)data;
+        }
+
+        public ContentResult Save(int? id, FormCollection actionValues)
+        {
+            var action = new DataAction(actionValues);
+
+            try
+            {
+                var changedEvent = (bokning)DHXEventsHelper.Bind(typeof(bokning), actionValues);
+                var data = new dsu3_dsu3DataContext();
+
+
+                switch (action.Type)
+                {
+                    case DataActionTypes.Insert:
+                        //do insert
+                        data.boknings.InsertOnSubmit(changedEvent);
+                        break;
+                    case DataActionTypes.Delete:
+                        changedEvent = data.boknings.SingleOrDefault(EventArgs => EventArgs.id == action.SourceId);
+                        break;
+                    default:// "update"                          
+                        var eventToUpdate = data.boknings.SingleOrDefault(EventArgs => EventArgs.id == action.SourceId);
+                        DHXEventsHelper.Update(eventToUpdate, changedEvent, new List<string>() { "id" });
+                        break;
+                }
+                data.SubmitChanges();
+                action.TargetId = changedEvent.id;
+            }
+            catch
+            {
+                action.Type = DataActionTypes.Error;
+            }
+            return (ContentResult)new AjaxSaveResponse(action);
+        }
+    }
+}
+
