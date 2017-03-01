9889d67e1266aa3ad0fe7cfbd9fe48216ed7f0bb
Tonny Svensson <tonnysvensson85@hotmail.se>
2017-03-01 10:23:42 +0100

    yo

--- Golf 3 MVC/Models/SchedulerModel.cs
+++ Golf 3 MVC/Models/SchedulerModel.cs
@@ -9,7 +9,8 @@ namespace Golf_3_MVC.Models
         public string text { get; set; }
         public DateTime start_date { get; set; }
         public DateTime end_date { get; set; }
-        public int golf_id { get; set; }
+
+        public string golf_id = "123919_182";
 
     }
 }
