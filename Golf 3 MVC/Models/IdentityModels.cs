be3806e3f3f947b447dfa4585b53830c9e8f94b0
Victor Olsson <olleolsson100@gmail.com>
2017-03-01 08:46:20 +0100

    Fixat kopplingen mellan identity och vår databas

--- Golf 3 MVC/Models/IdentityModels.cs
+++ Golf 3 MVC/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Golf_3_MVC.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("dsu3ConnectionString", throwIfV1Schema: false)
         {
         }
 
