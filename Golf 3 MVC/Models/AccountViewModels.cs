340502c5aa1695200d9fcfc9f640e37e65df018c
Victor Olsson <olleolsson100@gmail.com>
2017-03-01 13:56:55 +0100

    Fixat så man kan registrera sig och logga in med golf-id

--- Golf 3 MVC/Models/AccountViewModels.cs
+++ Golf 3 MVC/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace Golf_3_MVC.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "golfID")]
+        public string golfID { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -65,9 +64,8 @@ namespace Golf_3_MVC.Models
     public class RegisterViewModel
     {
         [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        [Display(Name = "golfID")]
+        public string golfID { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
