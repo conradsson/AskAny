137d592548f15bc4efea454690a3f1593a7de5ba
Hischam El Yahiaoui <hiel1500@student.miun.se>
2017-03-03 09:20:57 +0100

    Kalendern funkar

--- Golf 3 MVC/Controllers/CalendarController.cs
+++ Golf 3 MVC/Controllers/CalendarController.cs
@@ -43,110 +43,110 @@ namespace Golf_3_MVC.Controllers
         }
 
 
-        //public ContentResult Data()
-        //{
-        //    return (new SchedulerAjaxData(
-        //        new  dsu3Entities().boknings
-        //            .Select(e => new { e.id, e.text, e.start_date, e.end_date, e.golf_id })
-        //        )
-        //    );
-
-
-        //}
         public ContentResult Data()
         {
-            try
-            {
-                var details = ds.boknings.ToList();
-                return new SchedulerAjaxData(details);
+            return (new SchedulerAjaxData(
+                new dsu3Entities().boknings
+                    .Select(e => new { e.id, e.text, e.start_date, e.end_date, e.golf_id })
+                )
+            );
 
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
 
         }
+        //////public ContentResult Data()
+        //////{
+        //////    try
+        //////    {
+        //////        var details = ds.boknings.ToList();
+        //////        return new SchedulerAjaxData(details);
 
+        //////    }
+        //////    catch (Exception ex)
+        //////    {
+        //////        throw ex;
+        //////    }
+
+        //////}
+
+        //////public ContentResult Save(int? id, FormCollection actionValues)
+
+        //////{
+
+        //////    var action = new DataAction(actionValues);
+
+        //////    try
+        //////    {
+        //////        var changedEvent = (bokning)DHXEventsHelper.Bind(typeof(bokning),actionValues);
+
+        //////        switch (action.Type)
+        //////        {
+        //////            case DataActionTypes.Insert:
+        //////                bokning EV = new bokning();
+        //////                EV.id = 110;
+        //////                EV.start_date = changedEvent.start_date;
+        //////                EV.end_date = changedEvent.end_date;
+        //////                EV.text = changedEvent.text;
+        //////                EV.golf_id = "12319_182";
+        //////                ds.boknings.Add(EV);
+        //////                ds.SaveChanges();
+        //////                break;
+        //////            case DataActionTypes.Delete:
+        //////                var details = ds.boknings.Where(x => x.id == id).FirstOrDefault();
+        //////                ds.boknings.Remove(details);
+        //////                ds.SaveChanges();
+        //////                break;
+        //////            default:// "update"    
+        //////                var data = ds.boknings.Where(x => x.id == id).FirstOrDefault();
+        //////                data.start_date = changedEvent.start_date;
+        //////                data.end_date = changedEvent.end_date;
+        //////                data.text = changedEvent.text;
+        //////                ds.SaveChanges();
+        //////                break;
+        //////        }
+        //////    }
+        //////    catch
+        //////    {
+        //////        action.Type = DataActionTypes.Error;
+        //////    }
+        //////    return (ContentResult)new AjaxSaveResponse(action);
+        //////}
         public ContentResult Save(int? id, FormCollection actionValues)
-
         {
-
             var action = new DataAction(actionValues);
+            var changedEvent = DHXEventsHelper.Bind<bokning>(actionValues);
+            var entities = new dsu3Entities();
 
             try
             {
-                var changedEvent = (bokning)DHXEventsHelper.Bind(typeof(bokning),actionValues);
+                //var changedEvent = DHXEventsHelper.Bind<bokning>(actionValues);
+                //var entities = new dsu3Entities();
 
                 switch (action.Type)
                 {
                     case DataActionTypes.Insert:
-                        bokning EV = new bokning();
-                        EV.id = 110;
-                        EV.start_date = changedEvent.start_date;
-                        EV.end_date = changedEvent.end_date;
-                        EV.text = changedEvent.text;
-                        EV.golf_id = "12319_182";
-                        ds.boknings.Add(EV);
-                        ds.SaveChanges();
+                        entities.boknings.Add(changedEvent);
+                        //entities.boknings.Add(bok);
                         break;
                     case DataActionTypes.Delete:
-                        var details = ds.boknings.Where(x => x.id == id).FirstOrDefault();
-                        ds.boknings.Remove(details);
-                        ds.SaveChanges();
+                        changedEvent = entities.boknings.FirstOrDefault(ev => ev.id == action.SourceId);
+                        entities.boknings.Remove(changedEvent);
                         break;
-                    default:// "update"    
-                        var data = ds.boknings.Where(x => x.id == id).FirstOrDefault();
-                        data.start_date = changedEvent.start_date;
-                        data.end_date = changedEvent.end_date;
-                        data.text = changedEvent.text;
-                        ds.SaveChanges();
+                    default:// "update"   
+                        var target = entities.boknings.Single(e => e.id == changedEvent.id);
+                        DHXEventsHelper.Update(target, changedEvent, new List<string> { "id" });
                         break;
                 }
+
+                entities.SaveChanges();
+                action.TargetId = changedEvent.id;
             }
-            catch
+            catch (Exception a)
             {
                 action.Type = DataActionTypes.Error;
             }
-            return (ContentResult)new AjaxSaveResponse(action);
+
+            return (new AjaxSaveResponse(action));
         }
-        //public ContentResult Save(int? id, FormCollection actionValues)
-        //{
-        //    var action = new DataAction(actionValues);
-        //    var changedEvent = DHXEventsHelper.Bind<bokning>(actionValues);
-        //    var entities = new dsu3Entities();
-
-        //    try
-        //    {
-        //        //var changedEvent = DHXEventsHelper.Bind<bokning>(actionValues);
-        //        //var entities = new dsu3Entities();
-
-        //        switch (action.Type)
-        //        {
-        //            case DataActionTypes.Insert:     
-        //                entities.boknings.Add(changedEvent);                      
-        //                //entities.boknings.Add(bok);
-        //                break;
-        //            case DataActionTypes.Delete:
-        //                changedEvent = entities.boknings.FirstOrDefault(ev => ev.id == action.SourceId);
-        //                entities.boknings.Remove(changedEvent);
-        //                break;
-        //            default:// "update"   
-        //                var target = entities.boknings.Single(e => e.id == changedEvent.id);
-        //                DHXEventsHelper.Update(target, changedEvent, new List<string> { "id" });
-        //                break;
-        //        }
-
-        //        entities.SaveChanges();
-        //        action.TargetId = changedEvent.id;
-        //    }
-        //    catch (Exception a)
-        //    {
-        //        action.Type = DataActionTypes.Error;
-        //    }
-
-        //    return (new AjaxSaveResponse(action));
-        //}
 
 
     }
