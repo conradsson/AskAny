395cdfd367e562d26bd8963b1eaaccd3992b6cb7
Johan Conradsson <conradsson1993@hotmail.com>
2017-03-06 09:42:24 +0100

    info i gränssnittet

--- Golf 3 MVC/Scripts/site.js
+++ Golf 3 MVC/Scripts/site.js
@@ -1,40 +1,49 @@
 ﻿$(document).ready(function () {
-    $(".navbar").hide(0).fadeIn(500)
-    $(".body-content").hide(0).delay(450).fadeIn(1000)
-    $(".carousel").hide(0).delay(450).fadeIn(1000)
-    $(".panel-banstatus").hide();
+    //$(".navbar").hide(0).fadeIn(500)
+    //$(".body-content").hide(0).delay(450).fadeIn(1000)
+    //$(".carousel").hide(0).delay(450).fadeIn(1000)
 
 
 
-    //  START FLIKEN-EFFEKTER
-    $(".startbtn").click(function () {
-        $(".panel-nyheter").show();
-        $(".panel-banstatus").hide();
-    });
-    $(".pill-nyheter").click(function () {
-        $(".panel-nyheter").show();
-        $(".panel-banstatus").hide();
-    });
 
 
-    $(window).scroll(function () {   // Return-to-top Funktion
-        if ($(this).scrollTop() >= 400) {
-            $('#return-to-top').fadeIn(200);
-        } else {
-            $('#return-to-top').fadeOut(200);
-        }
-    });
-    $('#return-to-top').click(function () {  
-        $('body,html').animate({
-            scrollTop: 0
-        }, 300);
-    });
-
-
-    var scrollTo = function (identifier, speed) {  // Scroll-To function
-        $('html, body').animate({
-            scrollTop: $(identifier).offset().top
-        }, speed || 750);
-    }
 });
 
+//  START FLIKEN-EFFEKTER
+$(".startbtn").click(function (e) {
+    e.preventDefault()
+    $(".panel-nyheter").show();
+    $(".panel-banstatus").hide();
+});
+
+$(".pill-nyheter").click(function (e) {
+    e.preventDefault()
+    $(".panel-nyheter").show();
+    $(".panel-banstatus").hide();
+});
+$(".pill-banstatus").click(function (e) {
+    e.preventDefault()
+    $(".panel-nyheter").hide();
+    $(".panel-banstatus").show();
+});
+
+
+//$(window).scroll(function () {   // Return-to-top Funktion
+//    if ($(this).scrollTop() >= 400) {
+//        $('#return-to-top').fadeIn(200);
+//    } else {
+//        $('#return-to-top').fadeOut(200);
+//    }
+//});
+//$('#return-to-top').click(function () {
+//    $('body,html').animate({
+//        scrollTop: 0
+//    }, 300);
+//});
+
+
+//var scrollTo = function (identifier, speed) {  // Scroll-To function
+//    $('html, body').animate({
+//        scrollTop: $(identifier).offset().top
+//    }, speed || 750);
+//}
