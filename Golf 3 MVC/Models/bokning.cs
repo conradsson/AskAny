4f34587f052e22098ad40749761385fb5fa28bf8
Tonny Svensson <tonnysvensson85@hotmail.se>
2017-03-07 11:42:18 +0100

    yo

--- Golf 3 MVC/Models/bokning.cs
+++ Golf 3 MVC/Models/bokning.cs
@@ -14,11 +14,11 @@ namespace Golf_3_MVC.Models
     
     public partial class bokning
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public bokning()
-        {
-            this.medbokares = new HashSet<medbokare>();
-        }
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        //public bokning()
+        //{
+        //    this.medbokares = new HashSet<medbokare>();
+        //}
     
         public int id { get; set; }
         public string text { get; set; }
@@ -26,7 +26,7 @@ namespace Golf_3_MVC.Models
         public Nullable<System.DateTime> end_date { get; set; }
         public string golf_id { get; set; }
     
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<medbokare> medbokares { get; set; }
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual medbokare medbokares { get; set; }
     }
 }
