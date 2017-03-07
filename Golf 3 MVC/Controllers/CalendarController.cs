bacab4ca8f8b777d4ef076371d9e8e3cd0d0e49e
Hischam El Yahiaoui <hiel1500@student.miun.se>
2017-03-07 12:39:26 +0100

    Lightbox

--- Golf 3 MVC/Controllers/CalendarController.cs
+++ Golf 3 MVC/Controllers/CalendarController.cs
@@ -98,6 +98,22 @@ namespace Golf_3_MVC.Controllers
             //    EndDate = new DateTime(nu)
             //});
 
+            var check = new LightboxText("Highlighting", "Lägg till person");
+            sched.Lightbox.Add(check);
+
+            sched.Config.buttons_left =["dhx_save_btn", "dhx_cancel_btn", "locate_button"];
+
+            //sched.Config.buttons_right.Add(new EventButton
+            //{
+                
+            //    Label = "Lägg till medlem",
+            //    OnClick = "some_function",
+            //    Name = "location"
+                
+            //});
+
+            sched.Lightbox.AddDefaults();
+
             sched.Config.start_on_monday = true;
             sched.InitialView = "week";
             sched.EnableDynamicLoading(SchedulerDataLoader.DynamicalLoadingMode.Month);
