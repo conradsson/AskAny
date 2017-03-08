2a661b1762d5f1fe7e61831e65acbf0237509766
Hischam El Yahiaoui <hiel1500@student.miun.se>
2017-03-08 10:51:55 +0100

    Dropdown fungerar

---
+++ Golf 3 MVC/Models/CalendarBookings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DHTMLX.Scheduler;
+using DHTMLX.Common;
+using DHTMLX.Scheduler.Data;
+
+namespace Golf_3_MVC.Models
+{
+    public class CalendarBookings
+    {
+        public IEnumerable<bokning> minaBokningar { get; set; }
+
+
+        public DHXScheduler sched { get; set; }
+
+    }
+
+
+}
--- Golf 3 MVC/Models/bokning.cs
+++ Golf 3 MVC/Models/bokning.cs
@@ -14,19 +14,11 @@ namespace Golf_3_MVC.Models
     
     public partial class bokning
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public bokning()
-        {
-            this.medbokares = medbokares;
-        }
-    
         public int id { get; set; }
         public string text { get; set; }
         public System.DateTime start_date { get; set; }
         public Nullable<System.DateTime> end_date { get; set; }
         public string golf_id { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual medbokare medbokares { get; set; }
+        public Nullable<int> medlemsid { get; set; }
     }
 }
