8c2da349903d9627ee170a6e19fe6f141dc94d70
Johan Conradsson <conradsson1993@hotmail.com>
2017-03-09 10:58:53 +0100

    ny tabell Blocktime

--- Golf 3 MVC/Models/medlemsdata.Context.cs
+++ Golf 3 MVC/Models/medlemsdata.Context.cs
@@ -31,5 +31,6 @@ namespace Golf_3_MVC.Models
         public virtual DbSet<medbokare> medbokares { get; set; }
         public virtual DbSet<bokning> boknings { get; set; }
         public virtual DbSet<season> seasons { get; set; }
+        public virtual DbSet<blocktime> blocktimes { get; set; }
     }
 }
