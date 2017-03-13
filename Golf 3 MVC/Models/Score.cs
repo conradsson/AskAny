b72f2dc62359c454f3afc5dd56b41bed56f3b03e
viol1100 <viol1100@COSQ260PC10>
2017-03-13 14:23:51 +0100

    Fixat så att data genereras ifrån databasen til scorekortet

---
+++ Golf 3 MVC/Models/Score.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Golf_3_MVC.Models
+{
+    public class Score
+    {
+        public List<scorekort> scoreKort { get; set; }
+        public List<slope> slope { get; set; }
+       // public List<medlemmar> medlems { get; set; }
+
+    }
+}
