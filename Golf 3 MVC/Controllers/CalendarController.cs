721d027951cc8eaa3d90d522e02ee82f1a4cbbe7
Johan Conradsson <conradsson1993@hotmail.com>
2017-03-16 15:49:41 +0100

    end

--- Golf 3 MVC/Controllers/CalendarController.cs
+++ Golf 3 MVC/Controllers/CalendarController.cs
@@ -560,7 +560,7 @@ namespace Golf_3_MVC.Controllers
             sched.Lightbox.AddDefaults();
 
             sched.Config.start_on_monday = true;
-            sched.InitialView = "week";
+            sched.InitialView = "day";
             sched.EnableDynamicLoading(SchedulerDataLoader.DynamicalLoadingMode.Month);
             sched.Config.separate_short_events = true;
             sched.Config.hour_size_px = 100;
